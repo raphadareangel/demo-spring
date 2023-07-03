2747658fa198419745611f55190b0930daf0786b
raphadareangel <raphadareangel@hotmail.com>
2023-07-03 14:05:07 +0200

    Delete zarademoavanzado 11.49.02 directory
    cleaning reposytory

--- zarademoavanzado 11.49.02/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-insert into brand (name) values ('nike');
-insert into product (name) values ('shoes');
-INSERT INTO pricing (brand_id, start_date, end_date, price_list, priority, price, currency)
-VALUES (1, '2023-07-01T10:00:00', '2023-07-31T23:59:59', 'Summer Sale', 1, 25.99, 'USD');
-insert into PRODUCT_PRICING (product_id,pricing_id) values(1,1)
